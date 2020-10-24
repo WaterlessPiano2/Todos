a50f8277f192d73d1d0456e17e0399daac4682fe
Chad - ubuntu <scderya@gmail.com>
2020-10-24 22:39:03 +0100

    About page is more consistent with rest of the site

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,17 +1,34 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
+import { makeStyles, createStyles } from "@material-ui/core";
 
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      maxWidth: "1000px",
+      display: "block",
+      margin: "0 auto",
+    },
+    title: { textAlign: "center" },
+  })
+);
 const About = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <Typography variant="h3" component="h2" gutterBottom>
+    <div className={classes.root}>
+      <Typography
+        className={classes.title}
+        variant="h3"
+        gutterBottom
+      >
         About
       </Typography>
       <Typography variant="body1" gutterBottom>
         In this page we will talk about the reasons behind tech decisions. We
         will also talk about the features.
       </Typography>
-      <Typography variant="h4" gutterBottom>
+      <Typography className={classes.title} variant="h4" gutterBottom>
         Boiler Plate
       </Typography>
       <Typography variant="body1" gutterBottom>
@@ -21,7 +38,7 @@ const About = () => {
         have experince using it long time ago. It comes with TypeScript support
         and built in jest framework, so this saved me time at the start.
       </Typography>
-      <Typography variant="h4" gutterBottom>
+      <Typography className={classes.title} variant="h4" gutterBottom>
         Reusable Components
       </Typography>
       <Typography variant="body1" gutterBottom>
@@ -29,16 +46,13 @@ const About = () => {
         experience using it, and I like their documentations. Material-UI is
         based on Google's styling guide.
       </Typography>
-      <Typography variant="h5" gutterBottom>
+      <Typography className={classes.title} variant="h4" gutterBottom>
         Responsive
       </Typography>
       <Typography variant="body1" gutterBottom>
-        The menu is made responsive so it collapses in to a hamburger menu when
-        on a narrower screen. There is also a responsive container that every
-        page sit in that changes width when it being used on different screen
-        sizes.
+        This site is fully responsive, where user should not have to scroll sideways if they access this site with a smaller device.
       </Typography>
-      <Typography variant="h5" gutterBottom>
+      <Typography className={classes.title} variant="h4" gutterBottom>
         Themes
       </Typography>
       <Typography variant="body1" gutterBottom>
