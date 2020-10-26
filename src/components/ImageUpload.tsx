858b58aa35f177a2a740051920c0600a1d766688
Chad - ubuntu <scderya@gmail.com>
2020-10-26 20:34:33 +0000

    made image preview work!

--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -1,44 +1,48 @@
 import React, { useEffect, useState } from "react";
 import { useDropzone } from "react-dropzone";
+import { makeStyles, createStyles } from "@material-ui/core";
 
-const thumbsContainer = {
-  display: "flex",
-  flexDirection: "row",
-  flexWrap: "wrap",
-  marginTop: 16,
-};
+const useStyles = makeStyles(() =>
+  createStyles({
+    thumbsContainer: {
+      display: "flex",
+      flexDirection: "row",
+      flexWrap: "wrap",
+      marginTop: 16,
+    },
+    thumb: {
+      display: "inline-flex",
+      borderRadius: 2,
+      border: "1px solid #eaeaea",
+      marginBottom: 8,
+      marginRight: 8,
+      width: 100,
+      height: 100,
+      padding: 4,
+      boxSizing: "border-box",
+    },
 
-const thumb = {
-  display: "inline-flex",
-  borderRadius: 2,
-  border: "1px solid #eaeaea",
-  marginBottom: 8,
-  marginRight: 8,
-  width: 100,
-  height: 100,
-  padding: 4,
-  boxSizing: "border-box",
-};
+    thumbInner: {
+      display: "flex",
+      minWidth: 0,
+      overflow: "hidden",
+    },
+    img: {
+      display: "block",
+      width: "auto",
+      height: "100%",
+    },
+  })
+);
 
-const thumbInner = {
-  display: "flex",
-  minWidth: 0,
-  overflow: "hidden",
-};
-
-const img = {
-  display: "block",
-  width: "auto",
-  height: "100%",
-};
-
-function Previews(props) {
+function Previews(props: any) {
+  const classes = useStyles();
   const [files, setFiles] = useState([]);
   const { getRootProps, getInputProps } = useDropzone({
     accept: "image/*",
-    onDrop: (acceptedFiles) => {
+    onDrop: (acceptedFiles: any) => {
       setFiles(
-        acceptedFiles.map((file) =>
+        acceptedFiles.map((file: any) =>
           Object.assign(file, {
             preview: URL.createObjectURL(file),
           })
@@ -47,10 +51,10 @@ function Previews(props) {
     },
   });
 
-  const thumbs = files.map((file) => (
-    <div style={thumb} key={file.name}>
-      <div style={thumbInner}>
-        <img src={file.preview} style={img} />
+  const thumbs = files.map((file: any) => (
+    <div className={classes.thumb} key={file.name}>
+      <div className={classes.thumbInner}>
+        <img src={file.preview} className={classes.img} />
       </div>
     </div>
   ));
@@ -58,7 +62,7 @@ function Previews(props) {
   useEffect(
     () => () => {
       // Make sure to revoke the data uris to avoid memory leaks
-      files.forEach((file) => URL.revokeObjectURL(file.preview));
+      files.forEach((file: any) => URL.revokeObjectURL(file.preview));
     },
     [files]
   );
@@ -69,9 +73,9 @@ function Previews(props) {
         <input {...getInputProps()} />
         <p>Drag 'n' drop some files here, or click to select files</p>
       </div>
-      <aside style={thumbsContainer}>{thumbs}</aside>
+      <aside className={classes.thumbsContainer}>{thumbs}</aside>
     </section>
   );
 }
 
-<Previews />;
+export default Previews;
